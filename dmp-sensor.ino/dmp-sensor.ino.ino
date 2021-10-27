6997897708cdef42c2db2cf6436cd3a9cdf65ba8
SVRourke <samuelvanrourke@gmail.com>
2021-10-27 08:46:22 -0400

    simplified routine, added better outputs for client recognition of calibration

--- dmp-sensor.ino/dmp-sensor.ino.ino
+++ dmp-sensor.ino/dmp-sensor.ino.ino
@@ -23,6 +23,7 @@ uint8_t fifoBuffer[64]; // FIFO storage buffer
 Quaternion q;   // [w, x, y, z]         quaternion container
 float euler[3]; // [psi, theta, phi]    Euler angle container
 float ypr[3];   // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+VectorFloat gravity;    // [x, y, z]            gravity vector
 
 // ================================================================
 // ===               INTERRUPT DETECTION ROUTINE                ===
@@ -45,30 +46,24 @@ void setup()
     Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
 
     // initialize serial communication
-    // (115200 chosen because it is required for Teapot Demo output, but it's
-    // really up to you depending on your project)
     Serial.begin(38400);
-    while (!Serial)
-        ; // wait for Leonardo enumeration, others continue immediately
+    while (!Serial); // wait for Leonardo enumeration
 
     // initialize device
     mpu.initialize();
     pinMode(INTERRUPT_PIN, INPUT);
 
     // verify connection
-    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+    Serial.println(mpu.testConnection() ? F("calibration ready") : F("error"));
 
-    // wait for ready
-    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-    while (Serial.available() && Serial.read())
-        ; // empty buffer
-    while (!Serial.available())
-        ; // wait for data
-    while (Serial.available() && Serial.read())
-        ; // empty buffer again
+//    // wait for ready
+//    Serial.println(F());
+    while (Serial.available() && Serial.read());
+//    // wait for data
+    while (!Serial.available());
+//    // empty buffer again
+    while (Serial.available() && Serial.read());
 
-    // load and configure the DMP
-    Serial.println(F("Initializing DMP..."));
     devStatus = mpu.dmpInitialize();
 
     // supply your own gyro offsets here, scaled for min sensitivity
@@ -88,11 +83,10 @@ void setup()
         mpu.CalibrateGyro(6);
         mpu.PrintActiveOffsets();
         // turn on the DMP, now that it's ready
-        Serial.println(F("Enabling DMP..."));
         mpu.setDMPEnabled(true);
+        Serial.println("CALIBRATION FINISHED");
 
         // enable Arduino interrupt detection
-        Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
         attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
         mpuIntStatus = mpu.getIntStatus();
 
@@ -101,16 +95,13 @@ void setup()
 
         // get expected DMP packet size for later comparison
         packetSize = mpu.dmpGetFIFOPacketSize();
+        
     }
     else
     {
         // ERROR!
-        // 1 = initial memory load failed
-        // 2 = DMP configuration updates failed
-        // (if it's going to break, usually the code will be 1)
         Serial.print(F("DMP Initialization failed (code "));
         Serial.print(devStatus);
-        Serial.println(F(")"));
     }
 }
 
@@ -131,12 +122,13 @@ void loop()
         mpu.dmpGetQuaternion(&q, fifoBuffer);
         mpu.dmpGetGravity(&gravity, &q);
         mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-        //        Serial.print("yrp\t");
-        //        Serial.print(ypr[0] M");
-        Serial.printn(ypr[0] * 180 / M_PI);
-        Serial.print("\t");
-        Serial.print(ypr[1] * 180 / M_PI);
-        Serial.print("\t");
-        Serial.println(ypr[2] * 180 / M_PI);
+//
+//        Serial.print(ypr[0] * 180 / M_PI);
+//        Serial.print("\t");
+//        Serial.print(ypr[1] * 180 / M_PI);
+//        Serial.print("\t");
+//        Serial.println(ypr[2] * 180 / M_PI);
+
+        Serial.println(ypr[0] * 180 / M_PI, ypr[1] * 180 / M_PI, ypr[2] * 180 / M_PI);
     }
 }
